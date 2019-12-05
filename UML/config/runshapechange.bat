79030d25ae661af81b77ab95127176d1c16aebc1
jetgeo <knut@jetlundweb.no>
2019-12-05 12:23:24 +0100

    Improved mapping to ISO/TC211 and XSD

---
+++ UML/config/runshapechange.bat
@@ -0,0 +1,2 @@
+"java" -Dfile.encoding=UTF-8 -Dhttp.proxyHost=proxy.vegvesen.no -Dhttp.proxyPort=8080 -jar  "C:\DATA\Programvare\ShapeChange-2.8.0\ShapeChange-2.8.0.jar" -c "C:\DATA\GitHub\jetgeo\IFC2GML\UML\config\ShapeChangeConfiguration.xml"
+pause
